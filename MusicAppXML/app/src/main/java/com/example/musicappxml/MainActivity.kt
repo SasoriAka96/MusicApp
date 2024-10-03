bf4568fc0d208bcccbfa599f4453f478feb87491
SasoriAka96 <nahivallejo46@gmail.com>
2024-10-03 19:45:53 +0200

    Calculadora xml casi terminada

---
+++ MusicAppXML/app/src/main/java/com/example/musicappxml/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.musicappxml
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        
+    }
+}
+
