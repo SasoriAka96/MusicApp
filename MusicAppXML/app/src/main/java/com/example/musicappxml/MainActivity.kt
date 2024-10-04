f3e0d14b0dc6dfc2b62be065ef8d36c65cb44051
Nahikari Vallejo <nahivallejo46@gmail.com>
2024-10-05 01:13:04 +0200

    music app terminado

--- MusicAppXML/app/src/main/java/com/example/musicappxml/MainActivity.kt
+++ MusicAppXML/app/src/main/java/com/example/musicappxml/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.musicappxml
 
 import android.os.Bundle
-import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var play = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val playButton = findViewById<ImageView>(R.id.play)
 
-        
+        playButton.setOnClickListener {
+            play = !play
+            if (play) {
+                playButton.setImageResource(R.drawable.pausecircle)
+            } else {
+                playButton.setImageResource(R.drawable.playcircle)
+            }
+        }
+
+        enableEdgeToEdge()
+
     }
 }
 
